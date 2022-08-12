7629b513034c2d97d27b4d19d25537a93b38653c
Nathan Burgess <nathans.email.address@gmail.com>
2022-08-11 19:12:25 -0700

    current poll will display

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-// import functions and grab DOM elements
+import { renderPoll } from "./render-utils.js";
 
-// let state
+const pollFormEl = document.querySelector('#poll-form');
+const currentPollEl = document.querySelector('#current-poll');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let currentPoll = {
+    question: '',
+    optionOne: '',
+    optionTwo: '',
+    optionOneVotes: 0,
+    optionTwoVotes: 0
+};
+
+pollFormEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(pollFormEl);
+    currentPoll.question = data.get('question-input');
+    currentPoll.optionOne = data.get('option-one-input');
+    currentPoll.optionTwo = data.get('option-two-input');
+    displayCurrentPoll();
+});
+
+function displayCurrentPoll() {
+    currentPollEl.textContent = '';
+    const titleEl = document.createElement('h2');
+    titleEl.textContent = 'Current Poll';
+    const newPoll = renderPoll(currentPoll);
+    currentPollEl.append(titleEl, newPoll);
+}
